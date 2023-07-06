4d5d1ece54db00b6dca5e8391527b889cdad0e93
Hoang Nguyen Phuoc Bao <ngpbhoang1406@gmail.com>
2023-07-06 10:31:39 +0200

    Final commit

--- resources/scenario1/scenario1.sql
+++ resources/scenario1/scenario1.sql
@@ -1,3 +1,3 @@
-INSERT INTO election(Election_id, description) VALUES ('Spain2023', 'Spain2023'), ('Swiss2023-1', 'Swiss vote quarter 1');
-INSERT INTO voter(Voter_id, name, ssn) VALUES ('Alice', 'Alice', 'Alicessn'), ('Bob', 'Bob', 'Bobssn'), ('Charlie', 'Charlie', 'Charliessn');
-INSERT INTO vote(Vote_id, elections, voters, value) VALUES ('1', 'Spain2023', 'Alice', 3), ('2', 'Swiss2023-1', 'Alice', 4), ('3', 'Swiss2023-1', 'Bob', 1);
+INSERT INTO Election (Election_id, description) VALUES ('Spain2023', 'Spain2023'), ('Swiss2023-1', 'Swiss vote quarter 1');
+INSERT INTO Voter (Voter_id, name, ssn) VALUES ('Alice', 'Alice', 'Alicessn'), ('Bob', 'Bob', 'Bobssn'), ('Charlie', 'Charlie', 'Charliessn');
+INSERT INTO Vote (Vote_id, elections, voters, value) VALUES ('1', 'Spain2023', 'Alice', 3), ('2', 'Swiss2023-1', 'Alice', 4), ('3', 'Swiss2023-1', 'Bob', 1);
